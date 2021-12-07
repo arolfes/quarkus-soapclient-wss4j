1f50d0163ad2badd014e70cbae6510a91dc0169f
arolfes <arolfes@users.noreply.github.com>
2021-12-07 15:02:53 +0100

    reading username and password from properties instead hardcoded inside UsernameTokenPasswordClientCallback

--- src/main/kotlin/info/novatec/examples/soapclient/quarkus/soap/UsernameTokenPasswordClientCallback.kt
+++ src/main/kotlin/info/novatec/examples/soapclient/quarkus/soap/UsernameTokenPasswordClientCallback.kt
@@ -1,28 +1,24 @@
 package info.novatec.examples.soapclient.quarkus.soap
 
-import javax.enterprise.context.ApplicationScoped
-import org.apache.wss4j.common.ext.WSPasswordCallback
 import javax.security.auth.callback.Callback
 import javax.security.auth.callback.CallbackHandler
-import org.eclipse.microprofile.config.inject.ConfigProperty
+import org.apache.wss4j.common.ext.WSPasswordCallback
+import org.eclipse.microprofile.config.ConfigProvider
 
-//@ApplicationScoped
 class UsernameTokenPasswordClientCallback : CallbackHandler {
 
-//    @ConfigProperty(name = "wss4j.username")
-//    lateinit var wss4jUsername: String
-//
-//    @ConfigProperty(name = "wss4j.password")
-//    lateinit var wss4jPassword: String
-
     override fun handle(callbacks: Array<out Callback>) {
-        for (callback in callbacks) {
-            val wpc = callback as WSPasswordCallback
-            if (wpc.identifier == "dummyUserName") {
-                wpc.password = "dummyPassword"
-                return
-            }
+        val username = ConfigProvider.getConfig().getValue(WSS4J_USERNAME_PROP_KEY, String::class.java)
+        val password = ConfigProvider.getConfig().getValue(WSS4J_PASSWORD_PROP_KEY, String::class.java)
+        val wpc = callbacks[0] as WSPasswordCallback
+        if (wpc.identifier == username) {
+            wpc.password = password
         }
+    }
+
+    companion object {
+        private const val WSS4J_USERNAME_PROP_KEY = "wss4j.username"
+        private const val WSS4J_PASSWORD_PROP_KEY = "wss4j.password"
     }
 
 }
